3abe81cb4334c6aa8b355d1f55383dc9806fcedb
Kevin Languasco <kevin+github@languas.co>
2021-09-26 16:26:21 -0500

    refactor: use baseUrl with the src folder

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@
  */
 import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-import theme from "../config/theme";
+import theme from "common/theme";
 
 export default class Document extends NextDocument {
   render() {
